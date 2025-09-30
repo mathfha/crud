6923816150b76223ecc7de25f411dd820c503193
Aditya Rahman <111065815+mathfha@users.noreply.github.com>
2025-09-30 19:08:14 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $server = "localhost";
-$user = "adit";
-$password = "passwordku";
+$user = "";
+$password = "";
 $nama_database = "dbpus";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
